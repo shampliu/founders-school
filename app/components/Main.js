0e9e9b982e4fb540ba8ae324cabb4510d0047b03
Chang Liu <shampliu@gmail.com>
2017-03-01 03:12:34 -0800

    About component + clip path for slanted background

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,11 +1,15 @@
 var React = require('react');
 import Header from './Header';
+import About from './About';
+require('../scss/Main.scss');
 
 export default class Main extends React.Component {
   render() {
     return (
-
-      <Header />
+      <div>
+        <Header />
+        <About />
+      </div>
     )
   }
 };
