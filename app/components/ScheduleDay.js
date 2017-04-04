ad94831ad7e6c9d8b7d6cb92f03ff99d173f7228
Alexander Fong <alpfong@gmail.com>
2017-04-03 22:51:28 -0700

    Made schedule responsive

--- app/components/ScheduleDay.js
+++ app/components/ScheduleDay.js
@@ -1,21 +1,79 @@
 import React from 'react';
 import '../scss/Schedule.scss';
 
+let web_styles = {
+  "row1": {
+    "first_sessions": {
+      width: "29.9%"
+    },
+    "second_sessions": {
+      width: "40%"
+    },
+    "keynote1": {
+      width: "10%"
+    },
+    "keynote2": {
+      width: "9.5%"
+    }
+  },
+  "row2": {
+    "first_sessions": {
+      marginLeft: "10%",
+      width: "29.97%"
+    },
+    "second_sessions": {
+      marginLeft: "10%",
+      width: "39.97%"
+    },
+  }
+}
+
+let mobile_styles = {
+  "row1": {
+    "first_sessions": {
+      height: "30%"
+    },
+    "second_sessions": {
+      height: "40%"
+    },
+    "keynote1": {
+    },
+    "keynote2": {
+    }
+  },
+  "row2": {
+    "first_sessions": {
+      marginTop: "37.5px",
+      height: "30%",
+    },
+    "second_sessions": {
+      marginTop: "37.5px",
+      height: "40%",
+      backgroundColor: "#C44C60",
+    },
+  }
+}
+
 export default class ScheduleDay extends React.Component {
   render() {
+    let active_style = web_styles;
+    if (window.innerHeight < 1030) {
+      active_style = mobile_styles;
+    }
+
     return (
       <div className="schedule-day">
         <div className="timeline-table">
           <div className="timeline-row">
-            <div className="timeline-block">Keynote 1</div>
-            <div className="timeline-block" style={{width:"29.9%"}}>First Sessions</div>
+            <div className="timeline-block" style={active_style["row1"]["keynote1"]}>Keynote 1</div>
+            <div className="timeline-block" style={active_style["row1"]["first_sessions"]}>First Sessions</div>
             <div className="timeline-block">Lunch</div>
-            <div className="timeline-block" style={{width:"40%"}}>Second sessions</div>
-            <div className="timeline-block" style={{width:"9.5%"}}>Keynote 2</div>
+            <div className="timeline-block" style={active_style["row1"]["second_sessions"]}>Second sessions</div>
+            <div className="timeline-block" style={active_style["row1"]["keynote2"]}>Keynote 2</div>
           </div>
           <div className="timeline-row">
-            <div className="timeline-block" style={{marginLeft:"10%", width:"29.97%"}}>First Sessions</div>
-            <div className="timeline-block" style={{marginLeft:"10%", width:"39.97%"}}>Second sessions</div>
+            <div className="timeline-block" style={active_style["row2"]["first_sessions"]}>First Sessions</div>
+            <div className="timeline-block" style={active_style["row2"]["second_sessions"]}>Second sessions</div>
           </div>
         </div>
       </div>
