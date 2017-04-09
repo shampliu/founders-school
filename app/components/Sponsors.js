baf143087ed096cf5e16a9e2f70c04b9825005a4
Alexander Fong <alpfong@gmail.com>
2017-04-08 22:43:55 -0700

    Add sponsors

--- app/components/Sponsors.js
+++ app/components/Sponsors.js
@@ -7,15 +7,21 @@ export default class Sponsors extends Component {
 
   render() {
     return (
-      <div id="sponsors" className='sponsors'>
+      <div id="sponsors" className="sponsors">
         <div className="containers">
           <div className="header-title">Sponsors</div>
-          <div className="sponsors-grid section">
+          <div className="section">
+            <div className="sponsors-grid">
+              <div className="sponsor-tile"><img src="./assets/sponsors/startupucla.jpg"/></div>
+              <div className="sponsor-tile"><img src="./assets/sponsors/blackstone.png"/></div>
+              <div className="sponsor-tile" id="reslife"><img src="./assets/sponsors/reslife.png"/></div>
+            </div>
+            <div className="sponsors-grid lower">
+              <div className="sponsor-tile"><img src="./assets/sponsors/upe.png"/></div>
+              <div className="sponsor-tile"><img src="./assets/sponsors/acm.png"/></div>
+            </div>
+          </div>
         </div>
-      </div>
-
-
-
       </div>
     )
   }
